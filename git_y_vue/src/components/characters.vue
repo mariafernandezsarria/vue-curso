3b000193e1fc293b8bf5288818a377aa93974606
mariafernandezsarria <mariafernandezsarria@gmail.com>
2025-07-16 14:27:35 +0200

    Terminado hasta que se me ocurra alguna tonteria nueva que hacer

--- git_y_vue/src/components/characters.vue
+++ git_y_vue/src/components/characters.vue
@@ -13,31 +13,42 @@ const volverEpisodios = () => {
   router.push('/episodios');
 };
 
-onMounted(() => {
+onMounted(async () => {
   const id = route.params.id;
-  console.log(id);
-  axios.get(`https://rickandmortyapi.com/api/episode/${id}`)
-    .then((response) => {
-      personajes.value = response.data.characters;
-    })
-    .catch((error) => {
-      console.error('Error al obtener personajes:', error);
-    });
+  try {
+    const response = await axios.get(`https://rickandmortyapi.com/api/episode/${id}`);
+    const urls = response.data.characters;
+
+    // Obtener los datos de cada personaje con Promise.all
+    const requests = urls.map(url => axios.get(url));
+    const responses = await Promise.all(requests);
+    personajes.value = responses.map(res => res.data); // ← Guardamos los objetos completos
+  } catch (error) {
+    console.error('Error al obtener personajes:', error);
+  }
 });
 </script>
 
 <template>
-  <h1>HOLA</h1>
+  <h1>Personajes capítulo</h1>
 
-  <ul>
-    <li v-for="(personaje, index) in personajes" :key="index">
-      {{ personaje }}
-    </li>
-  </ul>
+    <div v-if="personajes.length">
+        <div v-for="(personaje, index) in personajes" v-bind:key="index" class="card mb-3" style="width: 18rem;">
+            <img :src="personaje.image" class="card-img-top" :alt="personaje.name">
+            <div class="card-body">
+                <h5 class="card-title">{{ personaje.name }}</h5>
+                <p class="card-text">Especie: {{ personaje.species }}</p>
+                <p class="card-text">Estado: {{ personaje.status }}</p>
+                <p class="card-text">Origen: {{ personaje.origin.name }}</p>
+            </div>
+        </div>
+    </div>
 
-  <button @click="volverEpisodios" class="btn btn-success">
-    Volver a los episodios
-  </button>
+    <div v-else>
+        <p>Cargando personajes...</p>
+    </div>
+
+    <button @click="volverEpisodios" class="btn btn-success">Volver a los episodios</button>
 </template>
 
 <style>
