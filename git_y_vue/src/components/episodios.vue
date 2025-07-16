70dedc863de3f3fbea0cc67c5001409c2d1d8f7a
mariafernandezsarria <mariafernandezsarria@gmail.com>
2025-07-16 09:50:53 +0200

    quiero poder ver lo que estoy haciendo

--- git_y_vue/src/components/episodios.vue
+++ git_y_vue/src/components/episodios.vue
@@ -1,4 +1,18 @@
 <script setup>
+import {ref, onMounted} from 'vue'
+import axios from 'axios'
+
+const arrayDatos = ref([])
+
+onMounted (()=> {
+  axios.get('https://rickandmortyapi.com/api/episode')
+  .then(response=>
+    arrayDatos.value = response.data
+  )
+  .catch(error=>
+    console.log('error al tomar los datos',error)
+  )
+})
 
 </script>
 
@@ -13,6 +27,15 @@
      </div>
 
     <h1>Aquí voy a hacer lo de los episodios porque por qué no me voy a complicar la vida</h1>
+
+    <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
 
 </template>
 
